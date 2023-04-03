9759c22d89492c2f58b0af36f137f38e435ed7e7
karelcosta <93195857+karelcosta@users.noreply.github.com>
2023-04-03 10:31:55 -0300

    Add files via upload

--- 03-04/main.py
+++ 03-04/main.py
@@ -1,15 +1,12 @@
 class Vertices:
-    def __init__(self, no) -> None:
+    def __init__(self, no, G) -> None:
         self.no = no
         self.grau = 1
         self.prox = []
-        self.an = []
+        G.addVertice(self)
     
-    def adicionar_aresta(self, n, x = True):
+    def adicionar_arestav(self, n):
         self.prox.append(n)
-        self.grau += 1
-        if x:
-            n.adicionar_aresta(self, False)
 
 class Grafo:
     def __init__(self, n) -> None:
@@ -19,6 +16,13 @@ class Grafo:
     def addVertice(self, v):
         self.vertices.append(v)
         self.grau += v.grau
+    
+    def adicionar_aresta(self, n, m):
+        self.vertices[n-1].adicionar_arestav(self.vertices[m-1])
+        self.vertices[n-1].grau += 1
+        self.vertices[m-1].grau += 1
+        self.grau += 2
+
         
     def removerArvore(self):
         for i in self.vertices:
@@ -37,34 +41,22 @@ class Grafo:
             
                 
 G = Grafo(4)
-vertice_1 = Vertices(1)
-vertice_2 = Vertices(2)
-vertice_3 = Vertices(3)
-vertice_4 = Vertices(4)
-vertice_1.adicionar_aresta(vertice_2)
-vertice_2.adicionar_aresta(vertice_3)
-vertice_1.adicionar_aresta(vertice_4)
-G.addVertice(vertice_1)
-G.addVertice(vertice_2)
-G.addVertice(vertice_3)
-G.addVertice(vertice_4)
+v = []
+v.append(Vertices(1, G))
+v.append(Vertices(2, G))
+v.append(Vertices(3, G))
+v.append(Vertices(4, G))
+G.adicionar_aresta(1, 2)
+G.adicionar_aresta(2, 3)
+G.adicionar_aresta(1, 4)
+
 
 for i in G.vertices:
     for j in i.prox:
-        print(f"({i.no},{j.no})")
-print(G.grau)
-
-for i in G.vertices:
-    print(i.grau)
-
-print("@")
+        print(f'({i.no},{j.no})')
 
 G.centroArvore()
+
+print("centro: ")
 for i in G.vertices:
     print(i.no)
-
-print("@")
-
-for i in G.vertices:
-    print(i.grau)
-print(G.grau)
